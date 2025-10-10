9be7fc4217d0ce6e391d9c8873b3fc55f6ccd81c
Hariom Tirkamte <hariomtirkamte@gmail.com>
2025-10-10 23:14:33 +0530

    Add files via upload

---
+++ Single.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+class Base
+{
+    public:
+        int i,j;
+
+    void Fun()
+    {
+        cout<<"Inside Base Fun: \n";
+    }
+};
+class Derived : public Base
+{
+    public:
+        int x;
+
+        void Gun()
+        {
+            cout<<"Inside Derived Gun: \n";
+        }
+};
+int main()
+{
+    Base bobj;
+    Derived dobj;
+
+    cout<<"Size of base class object: "<<sizeof(bobj)<<"\n";
+    
+    cout<<"Size of Derived class object: "<<sizeof(dobj)<<"\n";
+
+    return 0;
+}
+
